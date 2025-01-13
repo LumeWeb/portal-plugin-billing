b7eac542bc41188c754789571fb438f8e43365f3
Derrick Hammer <derrick@derrickhammer.com>
2025-01-13 00:31:11 -0500

    wip

--- internal/service/billing_account.go
+++ internal/service/billing_account.go
@@ -2,13 +2,17 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-openapi/strfmt"
 	"github.com/killbill/kbcli/v3/kbclient/account"
+	"github.com/killbill/kbcli/v3/kbclient/tag_definition"
 )
 
 const AUTO_PAY_TAG_NAME = "AUTO_PAY_OFF"
 
 func (b *BillingServiceDefault) setAutoPay(ctx context.Context, accountID strfmt.UUID, enabled bool) error {
+	tagDefId, err := b.getAutoPayTagDefId(ctx)
+
 	tags, err := b.api.Account.GetAccountTags(ctx, &account.GetAccountTagsParams{
 		AccountID: accountID,
 	})
@@ -16,17 +20,17 @@ func (b *BillingServiceDefault) setAutoPay(ctx context.Context, accountID strfmt
 		return err
 	}
 
-	var tagDefId strfmt.UUID
+	var found bool
 
 	for _, tag := range tags.Payload {
 		if tag.TagDefinitionName == AUTO_PAY_TAG_NAME {
-			tagDefId = tag.TagDefinitionID
+			found = true
 			break
 		}
 	}
 
 	if enabled {
-		if tagDefId != "" {
+		if found {
 			_, err = b.api.Account.DeleteAccountTags(ctx, &account.DeleteAccountTagsParams{
 				AccountID: accountID,
 				TagDef:    []strfmt.UUID{tagDefId},
@@ -40,7 +44,7 @@ func (b *BillingServiceDefault) setAutoPay(ctx context.Context, accountID strfmt
 		}
 	}
 
-	if tagDefId == "" {
+	if !found {
 		_, err = b.api.Account.CreateAccountTags(ctx, &account.CreateAccountTagsParams{
 			AccountID: accountID,
 			Body:      []strfmt.UUID{tagDefId},
@@ -52,4 +56,20 @@ func (b *BillingServiceDefault) setAutoPay(ctx context.Context, accountID strfmt
 	}
 
 	return nil
+}
+
+func (b *BillingServiceDefault) getAutoPayTagDefId(ctx context.Context) (strfmt.UUID, error) {
+	tagDefs, err := b.api.TagDefinition.GetTagDefinitions(ctx, &tag_definition.GetTagDefinitionsParams{})
+
+	if err != nil {
+		return "", err
+	}
+
+	for _, tagDef := range tagDefs.Payload {
+		if *tagDef.Name == AUTO_PAY_TAG_NAME && tagDef.IsControlTag {
+			return tagDef.ID, nil
+		}
+	}
+
+	return "", fmt.Errorf("cannot find AUTO_PAY_OFF tag definition")
 }
