e499d13bd0cbce69d2887ce26b412114e613ec12
Derrick Hammer <derrick@derrickhammer.com>
2025-01-20 21:18:26 -0500

    wip

--- internal/api/messages/webhook.go
+++
@@ -1,6 +0,0 @@
-package messages
-
-import "go.lumeweb.com/portal-plugin-billing/internal/client/hyperswitch"
-
-// Alias the hyperswitch WebhookEvent type
-type WebhookEvent = hyperswitch.WebhookEvent
