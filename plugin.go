aaaf7e1beded6bc92ba2a41558598b54e103b073
Derrick Hammer <derrick@derrickhammer.com>
2025-10-19 07:33:54 -0400

    feat(config): add stripe publishable key and pricing table id to config
    * Added `PublishableKey` and `PricingTableID` fields to `StripeConfig`
    * Implemented `Schema()` and `Defaults()` methods for `StripeConfig`
    * Updated `ServiceConfig.Schema()` and `ServiceConfig.Defaults()` to remove stripe specific config
    * Added meta data to plugin for stripe publishable key and pricing table id

--- plugin.go
+++ plugin.go
@@ -1,12 +1,15 @@
 package billing
 
 import (
+	"fmt"
 	"go.lumeweb.com/portal-plugin-billing/build"
 	"go.lumeweb.com/portal-plugin-billing/internal"
 	"go.lumeweb.com/portal-plugin-billing/internal/api"
+	"go.lumeweb.com/portal-plugin-billing/internal/config"
 	"go.lumeweb.com/portal-plugin-billing/internal/db/migrations"
 	"go.lumeweb.com/portal-plugin-billing/internal/db/models"
 	"go.lumeweb.com/portal-plugin-billing/internal/service/billing"
+	pluginCore "go.lumeweb.com/portal-plugin-billing/core"
 	"go.lumeweb.com/portal/core"
 )
 
@@ -24,6 +27,18 @@ func init() {
 			return []core.APIExtensionFactory{
 				api.NewAPIExtension(),
 			}, nil
+		},
+		Meta: func(ctx core.Context, builder core.PortalMetaBuilder) error {
+			cfg := core.GetServiceConfig[*config.ServiceConfig](ctx, pluginCore.BILLING_SERVICE)
+			plugin, err := builder.Plugin(internal.PLUGIN_NAME)
+			if err != nil {
+				return fmt.Errorf("failed to get plugin meta builder for billing: %w", err)
+			}
+			
+			plugin.AddMeta("stripe_publishable_key", cfg.Stripe.PublishableKey)
+			plugin.AddMeta("stripe_pricing_table_id", cfg.Stripe.PricingTableID)
+			
+			return nil
 		},
 		Models: []any{
 			&models.WebhookEvent{},
